06d025a548130ac0804081fbc4991bd28d239093
Anastasya Ivanova <nastya1615325@gmail.com>
2023-02-08 16:45:53 +0400

    part 10.9 done!

--- src/main/java/practice/EmailList.java
+++ src/main/java/practice/EmailList.java
@@ -1,19 +1,32 @@
 package practice;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class EmailList {
 
+    TreeSet<String> colection = new TreeSet<>();
+
     public void add(String email) {
+
+        String regex = "[A-Z a-z 0-9]+[@][a-z]+[.][a-z]+";
+
+        if (email.matches(regex)) {
+            colection.add(email.toLowerCase());
+        }
+
         // TODO: валидный формат email добавляется, email это строка, она быть может любой
         // принять решение добавлять аргумент email или нет должен этот метод
     }
 
     public List<String> getSortedEmails() {
+
+        ArrayList<String> collectionList = new ArrayList<>();
+
+        for (String email : colection) {
+
+            collectionList.add(email);
+        }
         // TODO: возвращается сортированный список электронных адресов в алфавитном порядке
-        return new ArrayList<>();
+        return collectionList;
     }
 
 }
--- src/main/java/practice/Main.java
+++ src/main/java/practice/Main.java
@@ -1,33 +1,47 @@
+package practice;
+
 import java.util.Scanner;
 
 public class Main {
+
     public static final String WRONG_EMAIL_ANSWER = "Неверный формат email";
-    
-    /* TODO:
-        Пример вывода списка Email, после ввода команды LIST в консоль:
-        test@test.com
-        hello@mail.ru
-        - каждый адрес с новой строки
-        - список должен быть отсортирован по алфавиту
-        - email в разных регистрах считается одинаковыми
-           hello@skillbox.ru == HeLLO@SKILLbox.RU
-        - вывод на печать должен быть в нижнем регистре
-           hello@skillbox.ru
-        Пример вывода сообщения об ошибке при неверном формате Email:
-        "Неверный формат email"
-    */
+
+    private static EmailList function = new EmailList();
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
+
         while (true) {
+            System.out.println("Введите командру");
             String input = scanner.nextLine();
+            String[] words = input.split("\\s");
+
             if (input.equals("0")) {
                 break;
             }
-            
+
+            if (words[0].equals("ADD")) {
+
+                if (words.length == 2) {
+                    function.add(words[1]);
+
+                } else {
+                    System.out.println(WRONG_EMAIL_ANSWER);
+
+                }
+
+            } else if (input.equals("LIST")) {
+
+                for (String emil : function.getSortedEmails()) {
+                    System.out.println(emil);
+
+                }
+            } else {
+                System.out.println("Данной команды не существует");
+            }
             //TODO: write code here
-            
+
         }
     }
 }
